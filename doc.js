9d9c0dc0e2672c2e3fe6f559f7112d1a41b416ac
alex djoum <kammegnealex@gmail.com>
2020-10-08 14:00:33 +0200

    manipulation des typeof

--- doc.js
+++ doc.js
@@ -1,4 +1,41 @@
 //const header = document.getElementsByTagName("h2");
 
-const items = document.getElementsByClassName("item")
-console.log(items);
+
+// ici je constate que lorsqu'on fait met un "s" sur le document.getElementByTagName , en l'appellant dans console.log il ne faut pas oublier de mettre un crochet et d'ajouter 0 à l'interieur du crochet
+
+// ici je constate que lorsqu'on fait met un "s" sur le document.getElementByClass , en l'appellant dans console.log il ne faut pas oublier de mettre un crochet et d'ajouter 0 à l'interieur du crochet
+
+//const items = document.getElementsByClassName("item");
+
+/* const button = document.getElementById("submit");
+console.log(button);
+ */
+//lorsque je fais document.getElementById("submit") il retourne le premier élément avec pour id="submit" 
+
+//const header = document.querySelector("h2");
+
+//const header = document.querySelector("#submit");
+
+//const header = document.querySelector(".item");
+
+//const header = document.querySelector(".item");
+//const header = document.querySelector('#list #submit');
+
+
+const header = document.querySelector('#list');
+//const htem = document.getElementsByClassName('#list');
+
+//console.log(header.children);
+
+//console.log(header.childNodes);
+
+//console.log(htem);
+
+//la difference entre getElement et querrySelector est que avec le query selector on utilise les sélecteurs css
+
+console.log(typeof "zaza"); //
+console.log(typeof 12);  // retourne  number
+console.log(typeof { name: "Alex", age: 24});  //retourne  objet
+console.log(typeof [1, 2, 3]);   //un tableau est  objet
+console.log(typeof undefined); // le type  undefined
+console.log(typeof null);  //null  un objet//
